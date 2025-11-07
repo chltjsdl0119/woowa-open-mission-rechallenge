1f2c96d31fb6eaf5ff2b6ec2ab2441fcf1b91b40
Seungwon-Choi <bestwondev@gmail.com>
2025-11-08 00:24:47 +0900

    chore: spring boot 프로젝트 추가

---
+++ src/main/java/com/mapofmemory/MapOfMemoryApplication.java
@@ -0,0 +1,13 @@
+package com.mapofmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapOfMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapOfMemoryApplication.class, args);
+    }
+
+}
