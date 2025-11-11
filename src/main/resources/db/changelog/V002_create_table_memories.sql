bb9bbef346683f4bcac3091ce0fd2598da3e280e
Seungwon-Choi <bestwondev@gmail.com>
2025-11-11 16:31:27 +0900

    refactor: memories DDL 수정

--- src/main/resources/db/changelog/V002_create_table_memories.sql
+++ src/main/resources/db/changelog/V002_create_table_memories.sql
@@ -3,8 +3,11 @@
 -- changeset Seungwon-Choi:2 runInTransaction:false
 CREATE TABLE IF NOT EXISTS memories (
     id         BIGINT AUTO_INCREMENT PRIMARY KEY   NOT NULL COMMENT '기억 PK',
+    member_id  BIGINT                               NOT NULL COMMENT '회원 FK',
     title      VARCHAR(200)                        NOT NULL COMMENT '기억 제목',
     content    TEXT                                NOT NULL COMMENT '기억 내용',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '데이터 생성일자',
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '데이터 수정일자'
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '데이터 수정일자',
+
+    CONSTRAINT fk_memories_member FOREIGN KEY (member_id) REFERENCES members (id)
 );
